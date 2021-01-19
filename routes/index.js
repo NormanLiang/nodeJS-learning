0293bf19b994d2f7dc99f4a0edbc3f66f0ec3047
Norman Liang <nomalee07@gmail.com>
2021-01-19 22:08:52 +1100

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ var express = require('express');
 var router = express.Router();
 var robot = require('../lib/robot');
 var FUNS = require('../lib/funs');
-var module_define = require('../lib/def');
+var DEF = require('../lib/def');
 
+
+const RETURN_STATE = {
+    RETURN_REPORTING: 0,
+    RETURN_OK: 1,
+    RETURN_ERROR: 2
+}
 
 router.get('/', function(req, res, next) {
     res.render('index');
@@ -12,25 +18,81 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 
-    cmds = FUNS.parseCmd(req.body.cmd);
+    let cmds = FUNS.parseCmd(req.body.cmd);
+    let res_state = RETURN_STATE.RETURN_ERROR;
+    let pos_x, pos_y, facing, trace, report;
 
     for (i=0; i<cmds.length; i++)
     {
         if (robot.act(cmds[i]))
         {
-            let pos_x = robot.getPositionX();
-            let pos_y = robot.getPositionY();
-            let facing = module_define.getDirectionString(robot.getFacing());
-            let trace = robot.getTracing();
+            //it's at least a valid command
+            if (robot.getInfo(robot.Robot_Info.STATE) == robot.Robot_state.REPORTING) { 
+                res_state = RETURN_STATE.RETURN_REPORTING;
 
-            var report = `${pos_x},${pos_y},${facing}`;
-            res.send({'CMD':req.body.cmd, 'REPORT': report, 'TRACE': trace});
-//            res.send({'POS X': pos_x, 'POS Y': pos_y, 'FACING': facing});
-            break;
+                break;  //if received a 'REPORT', send response and ingnore the rest command
+
+            } else {
+                // command is valid, but not going to report pos
+                res_state = RETURN_STATE.RETURN_OK;
+            }
+        
         }
     }
+
+    switch (res_state) {
+
+        case RETURN_STATE.RETURN_REPORTING:
+            //goint to report the pos and facing
+            pos_x = robot.getInfo(robot.Robot_Info.POS).x;
+            pos_y = robot.getInfo(robot.Robot_Info.POS).y;
+            facing = DEF.getDirectionString(robot.getInfo(robot.Robot_Info.FACING));
+            trace = robot.getInfo(robot.Robot_Info.TRACE);
     
+            report = `${pos_x},${pos_y},${facing}`;
     
+            if (DEF.DEBUG) {
+                res.send({'CMD':req.body.cmd, 'REPORT': report, 'TRACE': trace});
+            } else {
+                res.send({'POS X': pos_x, 'POS Y': pos_y, 'FACING': facing});
+            }  
+            robot.setTracing('');          
+            break;
+
+        case RETURN_STATE.RETURN_OK:
+            if (DEF.DEBUG) {
+                if (robot.getInfo(robot.Robot_Info.ONTABLE)) {
+                    pos_x = robot.getInfo(robot.Robot_Info.POS).x;
+                    pos_y = robot.getInfo(robot.Robot_Info.POS).y;
+                    facing = DEF.getDirectionString(robot.getInfo(robot.Robot_Info.FACING));
+                    trace = robot.getInfo(robot.Robot_Info.TRACE);
+        
+                    report = `${pos_x},${pos_y},${facing}`;
+                    res.send({'CMD':req.body.cmd, 'REPORT': report, 'TRACE': trace});
+                    
+                } else {
+                    res.send({'CMD':req.body.cmd, 'RES': 'NOT ON TABLE: ERROR IN RELEASE CODE'});
+                }
+                
+            } else {
+                if (robot.getInfo(robot.Robot_Info.ONTABLE)) {
+                    res.send({'CMD':req.body.cmd, 'RES': 'OK'});
+                } else {
+                    res.send({'CMD':req.body.cmd, 'RES': 'ERROR'});
+                }
+                
+            }
+            robot.setTracing('');
+            break;
+
+        case RETURN_STATE.RETURN_ERROR:
+            res.send({'CMD':req.body.cmd, 'RES': 'ERROR'});
+            robot.setTracing('');
+            break;
+
+        default:
+            break;
+    }
 });
 
 
--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const robot = require('../lib/robot');
 const DEF = require('../lib/def');
 const FUNS = require('../lib/funs');
+const { table } = require('../log');
 
 const test_cases_valid_place = [
     'PLACE 0,0,NORTH',
@@ -18,6 +19,7 @@ const test_cases_invalid_place = [
     ' ',
     'PA 0,0,NORTH',
     '0,0,EAST',
+    'PLACE 0,0',
     'PLACE 0,5,WEST',
     'PLACE 5,0,EAST',
     'PLACE 0,0,NOATH',
@@ -95,8 +97,34 @@ const test_cases_report_positon = {
     'PLACE 3,3,WEST': {x:3, y:3, f:3}    
 }
 
+const test_cases_on_valid_command = [
+    null,
+    undefined,
+    '',
+    ' ',
+    'PA 0,0,NORTH',
+    '0,0,EAST',
+    'PLACE 0,0',
+]
 
 describe('#robot.js', ()=> {
+
+    describe('#isValidPlace()', ()=> {
+        it('isValidPlace() on testcases', ()=>{
+            for (i=0; i<test_cases_on_valid_command.length; i++) {
+//                console.log(test_cases_on_valid_command[i]);
+                valid = FUNS.isValidPlace(test_cases_on_valid_command[i]);
+                assert.strictEqual(valid, false);
+            }
+            
+            for (i=0; i<test_cases_valid_place.length; i++) {
+//                console.log(test_cases_valid_place[i]);
+                valid = FUNS.isValidPlace(test_cases_valid_place[i]);
+                assert.strictEqual(valid, true);
+            }
+                
+        });
+    });
 
     describe('#act()', ()=> {
 
@@ -111,18 +139,16 @@ describe('#robot.js', ()=> {
 
                 ret = robot.act(test_cases_valid_place[i]);
 
-                pos_x = robot.getPositionX();
-                pos_y = robot.getPositionY();
-                facing = robot.getFacing();   
-                
-                // console.log('valid: ', pos_x, pos_y, facing, ret);
-                
+                pos = robot.getInfo(robot.Robot_Info.POS);
+                pos_x = pos.x;
+                pos_y = pos.y;
+                facing = robot.getInfo(robot.Robot_Info.FACING);  
+                tables = robot.getInfo(robot.Robot_Info.ONTABLE); 
+                    
                 assert.strictEqual(pos_x, e_x);
                 assert.strictEqual(pos_y, e_y);  
                 assert.strictEqual(facing, e_f);
-                assert.strictEqual(ret, false);
-
-                // console.log('test on valid cases ' + i + ' success');
+                assert.strictEqual(ret, true);
             }
         });
 
@@ -130,17 +156,17 @@ describe('#robot.js', ()=> {
 
             for (i=0; i<test_cases_invalid_place.length; i++) {
 
-                e_x = robot.getPositionX();
-                e_y = robot.getPositionY();
-                e_f = robot.getFacing();
+                e_x = robot.getInfo(robot.Robot_Info.POS).x;
+                e_y = robot.getInfo(robot.Robot_Info.POS).y;
+                e_f = robot.getInfo(robot.Robot_Info.FACING);
 
                 ret = robot.act(test_cases_invalid_place[i]);
 
-                pos_x = robot.getPositionX();
-                pos_y = robot.getPositionY();
-                facing = robot.getFacing(); 
+                pos_x = robot.getInfo(robot.Robot_Info.POS).x;
+                pos_y = robot.getInfo(robot.Robot_Info.POS).y;
+                facing = robot.getInfo(robot.Robot_Info.FACING);
 
-                // console.log('11', pos_x, pos_y, facing);
+                // console.log('11', pos_x, pos_y, facing, ret, test_cases_invalid_place[i]);
                 assert.strictEqual(pos_x, e_x);
                 assert.strictEqual(pos_y, e_y);  
                 assert.strictEqual(facing, e_f);
@@ -151,33 +177,48 @@ describe('#robot.js', ()=> {
             
         });
 
-        it ('MOVE test', ()=>{
+        it ('MOVE test when the robot is off table', ()=>{
             robot.reset();
 
             for (i=0; i<test_cases_move.length; i++) {
                 ret = robot.act(test_cases_move[i]);
-                on_table = robot.getOntable();
-                assert.strictEqual(ret, false);
+
+                if (i != test_cases_move.length-1) {
+                    assert.strictEqual(ret, false);
+                } else {
+                    assert.strictEqual(ret, true);
+                }
+                on_table = robot.getInfo(robot.Robot_Info.ONTABLE);
                 assert.strictEqual(on_table, false);
                 // console.log('test on move before on table ' + i + ' success');
             }
 
             // console.log(test_cases_move_positon);
-            for (var key in test_cases_move_positon) {
 
+        });
+
+        it('move test when the robot is on table', ()=>{
+            robot.reset();
+            for (var key in test_cases_move_positon) {
                 // console.log('key is ', key);
                 // console.log('Value is', test_cases_move_positon[key]);
                 robot.act(key);
 
-                for (i=0; i<test_cases_move.length; i++) {
+                on_table = robot.getInfo(robot.Robot_Info.ONTABLE);
+                assert.strictEqual(on_table, true);
+
+                for (i=0; i<test_cases_move.length; i++) {    
                     ret = robot.act(test_cases_move[i]);
-                    on_table = robot.getOntable();
-                    assert.strictEqual(ret, false);
-                    assert.strictEqual(on_table, true);
+                    if (i != test_cases_move.length-1) {
+                        assert.strictEqual(ret, false);
+                    } else {
+                        assert.strictEqual(ret, true);
+                    }
                 }
-                pos_x = robot.getPositionX();
-                pos_y = robot.getPositionY();
-                facing = robot.getFacing();
+                pos_x = robot.getInfo(robot.Robot_Info.POS).x;
+                pos_y = robot.getInfo(robot.Robot_Info.POS).y;
+                facing = robot.getInfo(robot.Robot_Info.FACING);
+
                 assert.strictEqual(pos_x, test_cases_move_positon[key].x);
                 assert.strictEqual(pos_y, test_cases_move_positon[key].y);
                 assert.strictEqual(facing, test_cases_move_positon[key].f);
@@ -186,60 +227,93 @@ describe('#robot.js', ()=> {
             }
         });
 
-        it ('left test', ()=>{
+        it ('left test off table', ()=>{
             robot.reset();
 
             for (i=0; i<test_cases_rotate_left.length; i++) 
             {
                 ret = robot.act(test_cases_rotate_left[i]);
-                on_table = robot.getOntable();
-                assert.strictEqual(ret, false);
+                
+                // console.log('command check ' + i);
+                if (i != test_cases_rotate_left.length-1) {
+                    //make sure the last one is the valid left command, the others are invalid
+                    assert.strictEqual(ret, false);
+                } else {
+                    assert.strictEqual(ret, true);  
+                } 
+                on_table = robot.getInfo(robot.Robot_Info.ONTABLE);
+                // console.log('on table check');
                 assert.strictEqual(on_table, false);
                 // console.log('test on left rotating before on table ' + i + ' success');
             }
+        });
 
+        it('left test on table', ()=>{
             for (var key in test_cases_rotate_left_positon) {
                 // console.log(key);
+                robot.reset();
                 robot.act(key);
                 for (i=0; i<test_cases_rotate_left.length; i++) {
                     ret = robot.act(test_cases_rotate_left[i]);
-                    on_table = robot.getOntable();
-                    assert.strictEqual(ret, false);
+                    on_table = robot.getInfo(robot.Robot_Info.ONTABLE);
+                    // console.log('command valid check');
+                    if (i != test_cases_rotate_left.length-1) {
+                        assert.strictEqual(ret, false);
+                    } else {
+                        assert.strictEqual(ret, true);
+                    }
+                    // console.log('on table check');
                     assert.strictEqual(on_table, true);
                 }                
-                pos_x = robot.getPositionX();
-                pos_y = robot.getPositionY();
-                facing = robot.getFacing();
+                pos_x = robot.getInfo(robot.Robot_Info.POS).x;
+                pos_y = robot.getInfo(robot.Robot_Info.POS).y;
+                facing = robot.getInfo(robot.Robot_Info.FACING);
+
+                // console.log('position check');
                 assert.strictEqual(pos_x, test_cases_rotate_left_positon[key].x);
                 assert.strictEqual(pos_y, test_cases_rotate_left_positon[key].y);
                 assert.strictEqual(facing, test_cases_rotate_left_positon[key].f);
                 // console.log('test on left rotating after on table ' + key + ' success');
             }
-        });
+        }); 
 
-        it ('right test', ()=>{
+        it ('right test off table', ()=>{
             robot.reset();
-
             for (i=0; i<test_cases_rotate_right.length; i++) 
             {
                 ret = robot.act(test_cases_rotate_right[i]);
-                on_table = robot.getOntable();
-                assert.strictEqual(ret, false);
+                if (i != test_cases_rotate_right.length-1) {
+                    assert.strictEqual(ret, false);
+                } else {
+                    assert.strictEqual(ret, true);
+                }
+                on_table = robot.getInfo(robot.Robot_Info.ONTABLE);
                 assert.strictEqual(on_table, false);
                 // console.log('test on left rotating before on table ' + i + ' success');
             }
+        });
 
+        it ('right test on table', ()=>{
+            
             for (var key in test_cases_rotate_right_positon) {
+                robot.reset();
                 robot.act(key);
                 for (i=0; i<test_cases_rotate_right.length; i++) {
                     ret = robot.act(test_cases_rotate_right[i]);
-                    on_table = robot.getOntable();
-                    assert.strictEqual(ret, false);
+                    // console.log('valid check');
+                    if (i != test_cases_rotate_right.length-1) {
+                        assert.strictEqual(ret, false);
+                    } else {
+                        assert.strictEqual(ret, true);
+                    }
+                    on_table = robot.getInfo(robot.Robot_Info.ONTABLE);
+                    // console.log('on table check');
                     assert.strictEqual(on_table, true);
                 }                
-                pos_x = robot.getPositionX();
-                pos_y = robot.getPositionY();
-                facing = robot.getFacing();
+                pos_x = robot.getInfo(robot.Robot_Info.POS).x;
+                pos_y = robot.getInfo(robot.Robot_Info.POS).y;
+                facing = robot.getInfo(robot.Robot_Info.FACING);
+                // console.log('postion check');
                 assert.strictEqual(pos_x, test_cases_rotate_right_positon[key].x);
                 assert.strictEqual(pos_y, test_cases_rotate_right_positon[key].y);
                 assert.strictEqual(facing, test_cases_rotate_right_positon[key].f);
@@ -247,23 +321,30 @@ describe('#robot.js', ()=> {
             }
         });
 
-        it ('report test', ()=>{
+        it ('report test on table', ()=>{
             robot.reset();
-
             for (i=0; i<test_cases_report.length; i++) 
             {
                 ret = robot.act(test_cases_report[i]);
-                on_table = robot.getOntable();
-                assert.strictEqual(ret, false);
+                on_table = robot.getInfo(robot.Robot_Info.ONTABLE);
+                if (i != test_cases_report.length-1) {
+                    assert.strictEqual(ret, false);
+                } else {
+                    assert.strictEqual(ret, true);
+                }
                 assert.strictEqual(on_table, false);
                 // console.log('test on report before on table ' + i + ' success');
             }
+        });
 
+        it ('report test on table', ()=>{
             for (var key in test_cases_report_positon) {
+                robot.reset();
                 robot.act(key);
                 for (i=0; i<test_cases_report.length; i++) {
                     ret = robot.act(test_cases_report[i]);
-                    on_table = robot.getOntable();
+                    on_table = robot.getInfo(robot.Robot_Info.ONTABLE);
+
                     if (test_cases_report[i] == 'REPORT'){
                         assert.strictEqual(ret, true);
                     } else {
@@ -271,9 +352,10 @@ describe('#robot.js', ()=> {
                     }
                     assert.strictEqual(on_table, true);
                 }                
-                pos_x = robot.getPositionX();
-                pos_y = robot.getPositionY();
-                facing = robot.getFacing();
+                pos_x = robot.getInfo(robot.Robot_Info.POS).x;
+                pos_y = robot.getInfo(robot.Robot_Info.POS).y;
+                facing = robot.getInfo(robot.Robot_Info.FACING);
+
                 assert.strictEqual(pos_x, test_cases_report_positon[key].x);
                 assert.strictEqual(pos_y, test_cases_report_positon[key].y);
                 assert.strictEqual(facing, test_cases_report_positon[key].f);
@@ -327,14 +409,33 @@ describe('#robot.js', ()=> {
 
     describe('#isValidPlace() in funs.js', ()=>{
         it('isValidPlace()', ()=>{
-            assert.strictEqual(FUNS.isValidPlace('PLACE 0,0,NORTH'), true);
+            assert.strictEqual(FUNS.isValidPlace('PLACE 0,0,NORTH'), true); 
+        });
+
+        it('isValidPlace() on PLACE 0 0 NORTH', ()=>{
             assert.strictEqual(FUNS.isValidPlace('PLACE 0 0 NORTH'), false);
+        });
+
+        it('isValidPlace() on PLBCE 0,0,NORTH', ()=>{
             assert.strictEqual(FUNS.isValidPlace('PLBCE 0,0,NORTH'), false);
+        });
+
+        it('isValidPlace() on PLACE 0,0,NOATH', ()=>{
             assert.strictEqual(FUNS.isValidPlace('PLACE 0,0,NOATH'), false);
+        });
+
+        it('isValidPlace() on place 0,0,north', ()=>{
             assert.strictEqual(FUNS.isValidPlace('place 0,0,north'), true);
+        });
+
+        it('isValidPlace() on null', ()=>{
             assert.strictEqual(FUNS.isValidPlace(null), false);
+        });
+
+        it('isValidPlace() on undefined', ()=>{
             assert.strictEqual(FUNS.isValidPlace(undefined), false);
         });
+
     });
 
     // describe('#parseCmd() in funs.js', ()=>{
@@ -344,7 +445,7 @@ describe('#robot.js', ()=> {
     //     });
     // });
 
-    
+   
 
 });
 
